e44e6ca10d843dfe949a3394773a9a5f859cf39d
João Sousa <sousa1989joao@gmail.com>
2014-04-02 23:15:11 +0100

    caixa nova falta por os ficheiros do nordic bem sem sym link

--- spi_msp430.h
+++ spi_msp430.h
@@ -13,18 +13,18 @@
 //#define CS_PIN_BASE P1OUT
 #define CS_PORT		P2OUT
 #define CS_DIR		P2DIR
-#define CS_PIN 		BIT2
+#define CS_PIN 		BIT6
 
 #define SIMO_PIN    BIT7
 #define SOMI_PIN    BIT6
 #define SCLK_PIN    BIT5
 
 //#define CE_PIN_BASE P1OUT
-#define CE_PIN 		BIT3
+#define CE_PIN 		BIT7
 #define CE_PORT		P2OUT
 #define CE_DIR		P2DIR
 
-#define IRQ_PIN 	BIT0
+#define IRQ_PIN 	BIT3
 #define IRQ_PORT	P1OUT
 #define IRQ_DIR		P1DIR
 
